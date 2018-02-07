9caaf553bb5b003da2c1a7d31b4f719a48e05bc5
Yang-33 <kasai.yuta0810@gmail.com>
2018-02-08 00:46:37 +0900

    全部いれたった

--- PythonApplication1/atcid.py
+++ PythonApplication1/atcid.py
@@ -1,6 +1,30 @@
 import src
+import sys
+
+
+def GetfromfileURL(filename):
+    URL = []
+    with open(filename,'r') as File:
+        for line in File:
+            URL.append(line)
+    return URL
 
 if __name__ == '__main__':
-    URL = str(input())
-    ID = src.GetSubmitID.GetSubmitID(URL)
-    print(ID)
+    IDs = []
+    URLs = []
+    usege = 'Usage: python {} [-f FILE] [--help]'\
+            .format(__file__)
+    args = sys.argv
+    if len(args) == 3: # file mode
+        URLs = GetfromfileURL(args[2])
+        for URL in URLs:
+            IDs.append(src.GetSubmitID.GetSubmitID(URL))
+    elif len(args) == 1:
+        URL = str(input())
+        ID = src.GetSubmitID.GetSubmitID(URL)
+        IDs.append(ID)
+        URLs.append(URL)
+    else :
+        print(usege)
+
+    src.PrintIDs.PrintIDs(IDs,URLs)
--- PythonApplication1/src/__init__.py
+++ PythonApplication1/src/__init__.py
@@ -1,3 +1,4 @@
 from . import GetSubmitID
+from . import PrintIDs
 
 
