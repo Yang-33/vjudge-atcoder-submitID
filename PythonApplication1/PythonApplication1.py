19af7abd1e067a16fedb4a9828947c294f1852c2
Yang-33 <kasai.yuta0810@gmail.com>
2018-02-07 23:52:33 +0900

    parseの部分を書いた

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,26 @@
+#coding: UTF-8
+import requests, bs4
+import re
+
+def GetSubmitID(URL):
+    
+    URL = "https://abc075.contest.atcoder.jp/tasks/abc075_c"
+    res = requests.get(URL)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    submitbutton = soup.select('.btn.btn-primary.btn-large')
+    subidtxt = submitbutton[0]["href"]
+    pattern = r'[0-9]+'
+    SubmitID = re.findall(pattern,subidtxt)[0]
+    #print("Submit ID is,",SubmitID)
+    #print(type(SubmitID))
+    #<a class="btn btn-primary btn-large" href="/submit?task_id=3596"><span class="lang lang-selected"><span class="lang-en lang-child hidden-lang">Submit</span><span class="lang-ja lang-child">提出する</span></span></a>
+    
+    return SubmitID
+
+
+
+sub = "a"
+print(function(sub))
+
