0393a00cafaac3c039873b74df2b539a78ea6f57
Yang-33 <kasai.yuta0810@gmail.com>
2018-02-08 02:35:05 +0900

    beta版用に正規化を書いた

--- PythonApplication1/src/GetSubmitID.py
+++ PythonApplication1/src/GetSubmitID.py
@@ -2,8 +2,29 @@
 import requests, bs4
 import re
 
+def NormalizeURL(URL):
+    # beta って書いてあったらこれを正規化
+    betamatch = re.match(r"https://beta.atcoder.jp",URL)
+
+    if betamatch:
+        print("ベータだお")
+        Contest_and_Problem = re.search(r"https://beta.atcoder.jp/contests/([a-zA-Z0-9]+)/tasks/(.+)",URL)
+        print(Contest_and_Problem)
+        ContestName = (Contest_and_Problem.group(1))
+        ProblemName =  (Contest_and_Problem.group(2))
+        alphaURL = "https://" + ContestName + ".contest.atcoder.jp/tasks/" + ProblemName
+        print(alphaURL)
+        return alphaURL
+    else :
+        print("ベータではないお")
+
+        return URL
+
+
 def GetSubmitID(URL):
     
+
+
     res = requests.get(URL)
     res.raise_for_status()
 
@@ -16,3 +37,13 @@ def GetSubmitID(URL):
     
     return SubmitID
 
+#https://abc075.contest.atcoder.jp/tasks/abc075_c
+# ttps://CONTESTNAME.contest.atcoder.jp/tasks/PROBLEMNAME
+#https://beta.atcoder.jp/contests/abc075/tasks/abc075_c
+# ttps://bera.atcoder.jp/CONTESTNAME/tasks/PROBLEMNAME
+
+#みたいにおきかえればよい
+
+#txt = str(input())
+#URL = NormalizeURL(txt)
+#print(GetSubmitID(URL))
